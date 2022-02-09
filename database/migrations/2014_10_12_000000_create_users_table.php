5521a6f256804a45660a5f63fed9a1da1191ba84
格致开物 <vector25@outlook.com>
2022-02-10 00:10:22 +0800

    更改字段

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name',125);
+            $table->string('cnname',125)->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
